0b5a374ce119459a5c00f5bd238cf37063b8ab05
hyedni <hyedni@naver.com>
2024-04-24 22:40:58 +0900

    pheonix 프로젝트 생성

---
+++ pheonix/src/main/java/com/kh/pheonix/PheonixApplication.java
@@ -0,0 +1,13 @@
+package com.kh.pheonix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PheonixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PheonixApplication.class, args);
+	}
+
+}
---
+++ pheonix/src/test/java/com/kh/pheonix/PheonixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kh.pheonix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PheonixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
