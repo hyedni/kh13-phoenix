11920b67f4060580fc920e7ccf6bcc912b8ee3fa
ggingggang05 <yllee221b@gmail.com>
2024-05-03 15:42:31 +0900

    상품 수량 수정 코드 추가

--- pheonix/src/main/java/com/kh/pheonix/restcontroller/CartRestController.java
+++ pheonix/src/main/java/com/kh/pheonix/restcontroller/CartRestController.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -60,6 +61,16 @@ public class CartRestController {
 		List<CartProductVO> list = cartDao.cartProductList(CartUserId);
 		List<CartProductVO> imageSetUpList = imageService.cartProductPhotoUrlSetUp(list);
 		return imageSetUpList;
+	}
+	
+	//상품 수정
+	@PatchMapping("/")
+	public ResponseEntity<?> edit(@RequestBody CartDto cartDto) {
+		boolean result = cartDao.updateQty(cartDto);
+		if (result == false) {
+			return ResponseEntity.status(404).build();
+		}
+		return ResponseEntity.ok().build();
 	}
 	
 	@DeleteMapping("/")
