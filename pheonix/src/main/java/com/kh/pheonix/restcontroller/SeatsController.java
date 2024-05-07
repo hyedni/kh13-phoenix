4ff0a5b13f608fe00c697fedd8331f3e96d3ca61
kkk1234 <rlaalsrn285@naver.com>
2024-05-07 18:49:39 +0900

    좌석

---
+++ pheonix/src/main/java/com/kh/pheonix/restcontroller/SeatsController.java
@@ -0,0 +1,12 @@
+package com.kh.pheonix.restcontroller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin
+@RestController
+@RequestMapping("/seats")
+public class SeatsController {
+
+}
