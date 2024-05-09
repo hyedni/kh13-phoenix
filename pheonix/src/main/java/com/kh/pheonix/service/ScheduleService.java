d3c60c5efbc6c34eda0531f5adce4a7caf2438f9
hyedni <hyedni@naver.com>
2024-05-09 18:10:50 +0900

    0509

--- pheonix/src/main/java/com/kh/pheonix/service/ScheduleService.java
+++ pheonix/src/main/java/com/kh/pheonix/service/ScheduleService.java
@@ -1,9 +1,13 @@
 package com.kh.pheonix.service;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
+import com.kh.pheonix.dao.MovieDao;
 import com.kh.pheonix.dao.UserCertDao;
 
 @Service
@@ -12,11 +16,21 @@ public class ScheduleService {
 	@Autowired
 	private UserCertDao userCertDao;
 	
+	@Autowired
+	private MovieDao movieDao;
+	
 	@Scheduled(cron ="0 0 * * * *")//100초마다
 	public void clearExpiredCerts() {
 		System.out.println("5분 지난 인증번호 삭제합니다");
 		// 모든 만료된 인증번호를 삭제
 		userCertDao.deleteExpiredCert();
+	}
+	
+	@Scheduled(cron ="0 0 0 * * ?") //자정
+	public void updateMovieOn () {
+		LocalDate today = LocalDate.now();
+        String formattedDate = today.format(DateTimeFormatter.ISO_LOCAL_DATE); // yyyy-MM-dd 형태로 포맷
+		movieDao.updateMovieOn(formattedDate);
 	}
 	
 }
