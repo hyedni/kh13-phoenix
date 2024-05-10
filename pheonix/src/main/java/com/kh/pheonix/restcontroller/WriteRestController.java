96ca23519e2b685cd2f513df253f979b8fa74561
kwonseozero <kwonseozero@naver.com>
2024-05-10 18:06:36 +0900

    write, personal, lost

--- pheonix/src/main/java/com/kh/pheonix/restcontroller/WriteRestController.java
+++ pheonix/src/main/java/com/kh/pheonix/restcontroller/WriteRestController.java
@@ -1,5 +1,6 @@
 package com.kh.pheonix.restcontroller;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,7 +14,10 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.kh.pheonix.Vo.PersonalWriteVO;
+import com.kh.pheonix.dao.PersonalDao;
 import com.kh.pheonix.dao.WriteDao;
+import com.kh.pheonix.dto.PersonalDto;
 import com.kh.pheonix.dto.WriteDto;
 
 @CrossOrigin
@@ -24,6 +28,9 @@ public class WriteRestController {
 	@Autowired
 	private WriteDao writeDao;
 	
+	@Autowired
+	private PersonalDao personalDao;
+	
 	@GetMapping("/")//조회
 	public List<WriteDto> list() {
 		return writeDao.selectList();
@@ -31,6 +38,9 @@ public class WriteRestController {
 	
 	@PostMapping("/")//등록
 	public void insert(@RequestBody WriteDto writeDto) {
+		int sequence = writeDao.sequence();
+		writeDto.setWriteNo(sequence);
+		System.out.println(writeDto);
 		writeDao.insert(writeDto);
 	}
 	
@@ -55,6 +65,25 @@ public class WriteRestController {
 	public void insertReply(@RequestBody WriteDto writeDto) {
 	    writeDao.insertReply(writeDto);
 	}
-
+	
+	//1:1문의 + 답글 조회
+	@GetMapping("/combine/{personalId}")
+	public List<PersonalWriteVO> personalList(@PathVariable String personalId) {
+	    List<PersonalDto> list = personalDao.selectList();
+	    // PersonalDto를 PersonalWriteVO로 변환하는 코드가 필요합니다.
+	    List<PersonalWriteVO> resultList = convertToPersonalWriteVO(list);
+	    return resultList;
+	}
+	
+	private List<PersonalWriteVO> convertToPersonalWriteVO(List<PersonalDto> dtoList) {
+	    List<PersonalWriteVO> result = new ArrayList<>();
+	    // PersonalDto를 PersonalWriteVO로 변환하는 로직을 구현합니다.
+	    for (PersonalDto dto : dtoList) {
+	        PersonalWriteVO vo = new PersonalWriteVO();
+	        // PersonalDto의 필드 값을 PersonalWriteVO의 필드로 복사하거나 변환하는 등의 작업을 수행합니다.
+	        result.add(vo);
+	    }
+	    return result;
+	}
 	}
 
