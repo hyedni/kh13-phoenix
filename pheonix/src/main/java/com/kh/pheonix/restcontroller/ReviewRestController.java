8f7bd1fe0ee122cc01b1a49fd02bd5729b3394a1
ggingggang05 <yllee221b@gmail.com>
2024-05-13 13:50:07 +0900

    리뷰게시판 기능 구현 중(리뷰 등록, 영화별 리뷰 목록, 리뷰 삭제)

---
+++ pheonix/src/main/java/com/kh/pheonix/restcontroller/ReviewRestController.java
@@ -0,0 +1,55 @@
+package com.kh.pheonix.restcontroller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.kh.pheonix.Vo.UserReviewVO;
+import com.kh.pheonix.dao.ReviewDao;
+import com.kh.pheonix.dto.ReviewDto;
+
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
+
+@CrossOrigin
+@RestController
+@RequestMapping("/review")
+public class ReviewRestController {
+	
+	@Autowired
+	private ReviewDao reviewDao;
+	
+	//리뷰등록
+	@PostMapping("/")
+	public ResponseEntity<?> insert(@RequestBody ReviewDto reviewDto) {
+		boolean isValid = reviewDao.findReview(reviewDto.getUserBookingNo());
+		if(isValid) {
+			int sequence = reviewDao.sequence();
+			reviewDto.setReviewNo(sequence);
+			reviewDao.insert(reviewDto); //관람한 영화에 대한 첫 리뷰인 경우에만 등록
+			return ResponseEntity.ok().build();
+		}
+		return ResponseEntity.notFound().build();
+	}
+	
+	//영화별 리뷰 및 회원 정보
+	@GetMapping("/{movieNo}")
+	public List<UserReviewVO> reviewListByMovie(@PathVariable int movieNo) {
+		return reviewDao.listByMovie(movieNo);
+	}
+	
+	//리뷰 삭제
+	@DeleteMapping("/{reviewNo}")
+	public void delete(@PathVariable int reviewNo){
+		reviewDao.delete(reviewNo);
+	}
+
+	
+}
